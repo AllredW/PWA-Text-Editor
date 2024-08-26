0e0bed0a7860cd2f6e1b75c8c4f12be6b998c1ba
AllredW <allrewil@gmail.com>
2024-08-26 18:35:51 -0400

    updated webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -10,15 +10,47 @@ module.exports = () => {
   return {
     mode: 'development',
     entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
+      main: "./src/js/index.js",
+      install: "./src/js/install.js",
+      delete: "./src/js/delete.js",
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      // Webpack plugin that generates the html file and injects the bundles.
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "TextBox",
+        favicon: "./favicon.ico",
+      }),
+      // Injects the custom service worker
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      // Creates a manifest.json file.
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "JATE",
+        description: "Whatever's on your mind, put it on a page.",
+        favicon: "./favicon.ico",
+        // Theme and background color pulled from duotone-light.min.css
+        background_color: "#faf8f5",
+        theme_color: "#b29762",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
     module: {
